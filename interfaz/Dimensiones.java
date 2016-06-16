035292c861dfecdbc65227e978aa35378396da90
Segundo Fariña <segundofarina@me.com>
2016-06-15 23:52:44 -0300

    dimensions

---
+++ interfaz/Dimensiones.java
@@ -0,0 +1,12 @@
+package interfaz;
+
+public interface Dimensiones {
+	final static double TABLERO_ANCHO=500;
+	final static double TABLERO_ALTO=500;
+	
+	final static double CASILLERO_ALTO=62.5;
+	final static double CASILLERO_ANCHO=62.5;
+	
+	final static int TABLERO_FILAS=8;
+	final static int TABLERO_COLUMNAS=8;
+}
